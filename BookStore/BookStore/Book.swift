8345c231a08cbd9e481f1e32c2364e52606057bc
Alejandro Perez G <alex98.perez@hotmail.com>
2021-03-12 19:08:44 -0700

    Add files via upload

---
+++ BookStore/BookStore/Book.swift
@@ -0,0 +1,16 @@
+//
+//  Book.swift
+//  BookStore
+//
+//  Created by Alejandro Pérez Gómez on 05/03/21.
+//
+
+import Foundation
+
+class Book {
+    var title: String = ""
+    var author: String = ""
+    var description: String = ""
+    var year: Int = 0
+}
+
